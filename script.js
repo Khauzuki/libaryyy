4d3a4b45d505f1ac9d64d1eb582228e38c2a4e00
khauzuki <ts2288732@gmail.com>
2024-06-09 14:43:21 +0300

    first

---
+++ script.js
@@ -0,0 +1,26 @@
+const myLibrary = [];
+
+function Book(name) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.status = this.status;
+
+
+}
+
+function createNewBook() {
+
+}
+
+function addBookToLibrary() {
+  myLibrary.push(book);
+}
+
+function removeBookFromLibrary() {
+    
+  }
+
+function changeBookStatus() {
+    
+  }
