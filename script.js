d3b56f50667bff3a9703551ab2dcd7077c50d5c9
khauzuki <ts2288732@gmail.com>
2024-06-10 21:44:38 +0300

    make function display book on page

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
+//Declare empty array for library
 const myLibrary = [];
 
+//Object constructor
 function Book(name) {
-  this.name = name;
+  this.title = title;
   this.author = author;
   this.pages = pages;
-  this.status = this.status;
+  this.read = this.read;
 
-
+  //this.info = function(){
+  //  return (this.title + this.author + ", " + this.pages)
+  //}
 }
 
-function createNewBook() {
+// Function to display library array to cards
+function displayBookOnPage() {
+  const books = document.querySelector(".books");
 
+  // Loop over the library array and display to the cards
+  myLibrary.forEach((myLibrary) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    books.appendChild(card);
+    for (let key in myLibrary) {
+      console.log(`${key}: ${myLibrary[key]}`);
+      const para = document.createElement("p");
+      para.textContent = `${key}: ${myLibrary[key]}`;
+      card.appendChild(para);
+    }
+  });
 }
 
-function addBookToLibrary() {
+//Function for adding book to the array
+function addBookToLibrary(title, author, pages, read) {
+  let book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
 
-function removeBookFromLibrary() {
-    
-  }
+function createNewBook() {}
 
-function changeBookStatus() {
-    
-  }
+function removeBookFromLibrary() {}
+
+function changeBookStatus() {}
+
+console.log("hello");
